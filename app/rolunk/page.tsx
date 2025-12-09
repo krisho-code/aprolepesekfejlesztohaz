6c853c14ee3c6e78ecf029cdc431c1d183f967df
krisho-code <kristo.dominik.levente@gmail.com>
2025-12-09 14:04:08 +0100

    Security update: Next.js and React

--- app/rolunk/page.tsx
+++ app/rolunk/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import AboutSection from "../../components/AboutSection";
+import AboutSection from "@/components/AboutSection";
 import ServicesOverview from "@/components/about/ServicesOverview";
 import TeamSection from "@/components/about/TeamSection";
 import ContactSection from "@/components/home/ContactSection";
